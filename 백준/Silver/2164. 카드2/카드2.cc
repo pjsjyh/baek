0e66a1c521a60767bad360c381418f3e7769412e
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2023-02-22 22:18:07 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main() {
+	cin.sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+
+	cin >> n;
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		q.push(i + 1);
+	}
+	while (q.size() != 1) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
+	return 0;
+}
