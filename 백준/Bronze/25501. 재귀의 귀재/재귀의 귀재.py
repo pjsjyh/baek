9b24f8733869689494d642d112cdc134fe098d53
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2023-03-26 02:37:15 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 468 ms, Memory: 31316 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,15 @@
+import sys
+t = int(sys.stdin.readline())
+for _ in range(t):
+    string = sys.stdin.readline().rstrip()
+    cnt = 0
+    def recursion(string, l, r):
+        global cnt
+        cnt += 1
+        if l >= r: return 1
+        elif string[l] != string[r]: return 0
+        else: return recursion(string, l+1, r-1)
+
+    def isPalindrome(string):
+        return recursion(string, 0, len(string)-1)
+    print(isPalindrome(string), cnt)
