c70e71cbc975eb8906956e80749b8d70f4f6d784
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2024-11-13 16:36:15 +0900

    [Gold V] Title: 퇴사 2, Time: 728 ms, Memory: 19476 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -1,31 +1,24 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
+
 int main() {
     int n;
-    cin >> n;
-    
-    vector<int> t(n), p(n), sumnum(n + 1, 0);
-
-    // 상담 일정과 보수 입력 받기
-    for (int i = 0; i < n; i++) {
-        cin >> t[i] >> p[i];
+    cin>>n;
+    int t[n+1], p[n+1], sumnum[n+1]={};
+    for(int i=0;i<n;i++){
+        cin>>t[i]>>p[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        // 현재까지 계산된 최대 수익을 다음 날로 갱신
-        sumnum[i + 1] = max(sumnum[i + 1], sumnum[i]);
-        
-        // 상담을 진행할 수 있는 경우
+    for(int i=0;i<n;i++){
         if (i + t[i] <= n) {
             sumnum[i + t[i]] = max(sumnum[i + t[i]], sumnum[i] + p[i]);
         }
+        sumnum[i + 1] = max(sumnum[i + 1], sumnum[i]);
     }
 
-    // n일까지의 최대 수익 출력
-    cout << sumnum[n] << endl;
+    cout<<sumnum[n];
     return 0;
 }
