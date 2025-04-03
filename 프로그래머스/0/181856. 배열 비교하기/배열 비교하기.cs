b701eeb11ba449269f474bee566547904febb207
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2025-04-03 13:37:53 +0900

    [level 0] Title: 배열 비교하기, Time: 0.80 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
@@ -1,27 +1,16 @@
 using System;
-
+using System.Linq;
 public class Solution {
     public int solution(int[] arr1, int[] arr2) {
         int answer = 0;
-         if(arr1.Length>arr2.Length){
-             answer = 1;
-         }
-        else if(arr1.Length<arr2.Length){
-            answer = -1;
-        }
-        else{
-            int arr1sum=0, arr2sum=0;
-            for(int i=0;i<arr1.Length;i++){
-                arr1sum+=arr1[i];
-                arr2sum+=arr2[i];
-                    
-            }
+         if (arr1.Length != arr2.Length)
+            return arr1.Length > arr2.Length ? 1 : -1;
+         int arr1sum = arr1.Sum(), arr2sum = arr2.Sum();
             if(arr1sum>arr2sum){
                 answer = 1;
             }
             else if(arr1sum<arr2sum) answer = -1;
             else answer = 0;
-        }
         return answer;
     }
 }
