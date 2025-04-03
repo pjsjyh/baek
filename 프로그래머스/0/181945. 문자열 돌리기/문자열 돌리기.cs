b385c432a502f272883bdad392f1f7b44f5fb228
pjsjyh <120554629+pjsjyh@users.noreply.github.com>
2025-04-03 12:12:26 +0900

    [level 0] Title: 문자열 돌리기, Time: 27.32 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        for(int i=0;i<s.Length;i++){
+            Console.WriteLine(s[i]);
+        }
+    }
+}
